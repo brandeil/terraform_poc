0305f28597d25c3039a90eec9e20cdd12c93a17e
brandeil <glenb73@gmail.com>
2018-06-25 21:53:07 -0400

    variables

--- example.tf
+++ example.tf
@@ -1,10 +1,16 @@
 provider "aws" {
-  access_key = {}
-  secret_key = {}
-  region     = "us-east-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+resource "aws_instance" "ec2" {
+  ami           = "ami-a4dc46db"
   instance_type = "t2.micro"
+  subnet_id     = "subnet-ae3bfdf3"
+  key_name      = "MyEC2KeyPair"
+
+  tags {
+    Name = "${var.environment}-xyz"
+  }
 }
